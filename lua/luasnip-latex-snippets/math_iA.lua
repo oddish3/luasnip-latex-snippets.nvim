8e74c7f7db83deec83ab70655a4cb7e16d70f94c
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-02-06 12:26:21 -0500

    chore: ignore setminus for now

--- lua/luasnip-latex-snippets/math_iA.lua
+++ lua/luasnip-latex-snippets/math_iA.lua
@@ -123,7 +123,7 @@ function M.retrieve(is_math)
     parse_snippet({ trig = "floor", name = "floor" }, "\\left\\lfloor $1 \\right\\rfloor$0"),
     parse_snippet({ trig = "mcal", name = "mathcal" }, "\\mathcal{$1}$0"),
     parse_snippet({ trig = "//", name = "Fraction" }, "\\frac{$1}{$2}$0"),
-    parse_snippet({ trig = "\\\\\\", name = "setminus" }, "\\setminus"),
+    -- parse_snippet({ trig = "\\\\\\", name = "setminus" }, "\\setminus"),
     parse_snippet({ trig = "->", name = "to", priority = 100 }, "\\to "),
     parse_snippet({ trig = "-->", name = "long to", priority = 200 }, "\\longrightarrow "),
 
